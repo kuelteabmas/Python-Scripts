52aa6cf49be3844e37bad734399a6ff12c33f85a
kuelteabmas <fallmuhamed@gmail.com>
2023-07-03 18:31:50 -0400

    script description for ps1-rom-mgmt-on-psvita

--- ps1-rom-mgmt-on-psvita/ps1-rom-mgmt-on-psvita.py
+++ ps1-rom-mgmt-on-psvita/ps1-rom-mgmt-on-psvita.py
@@ -1,1 +1,12 @@
-#
+''' 
+Requirements:
+Each PS1 ROM needs to be of format .PBP file due to the Adreanaline requirements
+
+This script has a mission of:
+- fetch the list of ROMs in your folder (US roms only for now)
+- gets each ROM title's Title ID Type (NPUI) and Title ID Number (94244) and concatenate them (ie: NPUI94244) 
+- create a new folder titled with the newly formed Title ID
+- move said ROM into the newly created folder
+- rename the PBP ROM file to EBOOT.PBP
+'''
+
