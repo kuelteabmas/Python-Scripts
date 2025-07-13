563daceb5458235ec273ff2abf61a60a882c1aeb
kuelteabmas <fallmuhamed@gmail.com>
2025-07-13 19:57:24 -0400

    Draft

--- util/format_paragraphs/format_paragraphs.py
+++ util/format_paragraphs/format_paragraphs.py
@@ -8,9 +8,23 @@ def formatParagraphs():
 
     with open('input.txt', 'r') as file:
         lines = file.readlines()
+        numberOfLines = len(lines)
+        print('Total Number of lines:', numberOfLines)
 
-    lines = [line.replace('\n', ' ') for line in lines]
-    lines = [line.replace('(Risas)', '\n\n') for line in lines]
+    # i = 1
+    # while i < numberOfLines:
+    for i in range(numberOfLines):
+        if i % 5 == 0:
+            lines = [line.replace('\n', '\n\n') for line in lines]
+        # i += 1
+
+
+        # lines = [line.replace('\n', ' ') for line in lines]
+        # lines = [line.replace('(Risas)', '\n\n') for line in lines]
+
+        
+        # for i in range(7):
+            # lines = [line.replace('\n', '\n\n\n') for line in lines]
 
     # Write modified lines to a text file
     with open('formatted_paragraphs.txt', 'w') as file:
